18321f83f73aa543c87bf33d3d38f1ec4707a894
SmileTeacher <SmileTeacher@qq.com>
2018-01-02 18:42:29 +0800

    add some test code

--- src/test/java/com/ixuea/test/TestMathUtil.java
+++ src/test/java/com/ixuea/test/TestMathUtil.java
@@ -18,11 +18,14 @@ public class TestMathUtil {
         Assert.assertEquals(result,a);
     }
 
-    @Test
-    public void testMin(){
-        Assert.assertEquals(MathUtil.min(30,20),20);
-        Assert.assertEquals(MathUtil.min(10,6),6);
-        Assert.assertEquals(MathUtil.min(20,2),2);
-        Assert.assertEquals(MathUtil.min(30,1),1);
-    }
+@Test
+public void testMin(){
+    Assert.assertEquals(MathUtil.min(30,20),20);
+    Assert.assertEquals(MathUtil.min(10,6),6);
+    Assert.assertEquals(MathUtil.min(20,2),2);
+    Assert.assertEquals(MathUtil.min(30,1),1);
+
+//        补全测试
+    Assert.assertEquals(MathUtil.min(1,30),1);
+}
 }
