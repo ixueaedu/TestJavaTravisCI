3c98aeeb4f32a0aafcca822b8f62546588673e27
SmileTeacher <SmileTeacher@qq.com>
2018-01-02 19:02:03 +0800

    add repeat code

--- src/main/java/com/ixuea/test/MathUtil.java
+++ src/main/java/com/ixuea/test/MathUtil.java
@@ -4,24 +4,25 @@ public class MathUtil {
 
     /**
      * 返回a,b两个数中最大数的
+     *
      * @param a
      * @param b
      * @return
      */
-    public static int max(int a,int b) {
-        return a>b?a:b;
+    public static int max(int a, int b) {
+        return a > b ? a : b;
 
 //                return a>b?b:a;// 这是错误的
     }
 
 
-public static int min(int a,int b) {
-    if (a > b) {
-        //假设这里还有很多逻辑
-        return b;
-    } else {
-        //假设这里还有很多逻辑
-        return a;
+    public static int min(int a, int b) {
+        if (a > b) {
+            //假设这里还有很多逻辑
+            return b;
+        } else {
+            //假设这里还有很多逻辑
+            return a;
+        }
     }
-}
 }
